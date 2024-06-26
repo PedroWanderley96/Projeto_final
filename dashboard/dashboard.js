de68c72107f0a413455f70a18c6825b0e123c1aa
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2024-06-26 12:20:01 -0300

    refactor:ajustes nas referencias

--- dashboard/dashboard.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbarToggler = document.querySelector(".navbar-toggler");
-  const navbarCollapse = document.querySelector(".collapse.navbar-collapse");
-
-  navbarToggler.addEventListener("click", function () {
-    navbarCollapse.classList.toggle("show");
-  });
-});
