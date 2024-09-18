adc4e09d217a6cfb8be55f31dd99ce49fca207f2
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2024-09-18 20:23:35 -0300

    feature/alterando js header

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const navbarToggler = document.querySelector(".navbar-toggler");
   const navbarCollapse = document.querySelector(".collapse.navbar-collapse");
+  const navbarToggler = document.querySelector(".navbar-toggler");
 
   navbarToggler.addEventListener("click", function () {
     navbarCollapse.classList.toggle("show");
