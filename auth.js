8a2c40f8fda284078a666cf30bd46e97d7edc934
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2024-10-12 13:10:44 -0300

    Esconde links do dashboard e planilha no footer quando não logado

--- auth.js
+++ auth.js
@@ -1,24 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dashboardLink = document.querySelector("#dashboard-link");
-  const planilhaLink = document.querySelector("#planilha-link");
+  const dashboardLinkHeader = document.querySelector("#dashboard-link");
+  const planilhaLinkHeader = document.querySelector("#planilha-link");
   const loginLogoutLink = document.querySelector("#login-logout-link");
+
+  const dashboardLinkFooter = document.querySelector("#dashboard-link-footer");
+  const planilhaLinkFooter = document.querySelector("#planilha-link-footer");
 
   const isLogin = localStorage.getItem("email") !== null;
 
   if (isLogin) {
-    dashboardLink.style.display = "block";
-    planilhaLink.style.display = "block";
+    dashboardLinkHeader.style.display = "block";
+    planilhaLinkHeader.style.display = "block";
     loginLogoutLink.textContent = "Logout";
     loginLogoutLink.addEventListener("click", function (e) {
       e.preventDefault();
       localStorage.removeItem("email");
       localStorage.removeItem("password");
-      window.location.href = "index.html";
+      window.location.href = "../index.html";
     });
   } else {
-    dashboardLink.style.display = "none";
-    planilhaLink.style.display = "none";
+    dashboardLinkHeader.style.display = "none";
+    planilhaLinkHeader.style.display = "none";
     loginLogoutLink.textContent = "Login";
-    loginLogoutLink.href = "login/login.html";
+    loginLogoutLink.href = "../login/login.html";
+  }
+
+  if (dashboardLinkFooter && planilhaLinkFooter) {
+    if (isLogin) {
+      dashboardLinkFooter.style.display = "block";
+      planilhaLinkFooter.style.display = "block";
+    } else {
+      dashboardLinkFooter.style.display = "none";
+      planilhaLinkFooter.style.display = "none";
+    }
   }
 });
