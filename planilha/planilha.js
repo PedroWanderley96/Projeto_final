3244d0b0f5a103027d0df3bcb47a3f4da34ac97a
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2024-10-23 10:42:47 -0300

    ajuste no grafico da planilha

--- planilha/planilha.js
+++ planilha/planilha.js
@@ -1,25 +1,41 @@
-console.log('JS carregado')
+const ctx = document.getElementById("myChart").getContext("2d");
 
-const ctx = document.getElementById('myChart').getContext('2d');
-
-let labelsX = ["Resultado Geral", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-let valoresX = [280, 270, 800, 700, 650, 750, 450, 240, 900, 1000, 740, 950]
+let labelsX = [
+  "Resultado Geral",
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+let valoresX = [280, 270, 800, 700, 650, 750, 450, 240, 900, 1000, 740, 950];
 
 new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: {
-    labels: [labelsX],
-    datasets: [{
-      label: 'Lucro (R$)',
-      data: [valoresX],
-      borderWidth: 1
-    }]
+    labels: labelsX,
+    datasets: [
+      {
+        label: "Lucro (R$)",
+        data: valoresX,
+        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderWidth: 1,
+      },
+    ],
   },
   options: {
     scales: {
       y: {
-        beginAtZero: true
-      }
-    }
-  }
+        beginAtZero: true,
+      },
+    },
+  },
 });
